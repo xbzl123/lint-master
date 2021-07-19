d126cd8fe982d049631653cace78eab8501c8f5b
longyanghe <742781499@qq.com>
2021-07-19 17:14:48 +0800

    add function class

--- demo/app/src/main/java/com/twlk/lint/TVBean.java
+++ demo/app/src/main/java/com/twlk/lint/TVBean.java
@@ -3,4 +3,19 @@ package com.twlk.lint;
 public class TVBean {
     private int number;
     private String name;
+
+    public TVBean(int number, String name) {
+        this.number = number;
+        this.name = name;
+        int i = 0;
+        if (i > 0) {
+            if (i < 0) {
+                if (i == 0) {
+                    if (i < -1) {
+
+                    }
+                }
+            }
+        }
+    }
 }
